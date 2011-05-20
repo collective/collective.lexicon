f467d549e8cb1641c09def36f70b6ddd3cb7f710
Clayton Parker <clayton@sixfeetup.com>
2011-05-20 17:15:37 +0000

    pep8 and pyflakes cleanup
    git-svn-id: https://svn.plone.org/svn/collective/collective.vocabularymanager/trunk@239698 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/vocabularymanager/events.py
+++ collective/vocabularymanager/events.py
@@ -1,13 +1,17 @@
-from collective.vocabularymanager.interfaces import ITermRemovedEvent, IVocabularyRemovedEvent
+from zope.interface import implements
+from collective.vocabularymanager.interfaces import ITermRemovedEvent
+from collective.vocabularymanager.interfaces import IVocabularyRemovedEvent
+
 
 class TermRemovedEvent(object):
     implements(ITermRemovedEvent)
- 
+
     def __init__(self, vocab_id, term_id):
         self.vocab_id = vocab_id
         self.term_id = term_id
 
-class VocabularyRemovedEvent(ObjectEvent):
+
+class VocabularyRemovedEvent(object):
     implements(IVocabularyRemovedEvent)
 
     def __init__(self, vocab_id):
