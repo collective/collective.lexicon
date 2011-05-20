273cd7cbb94ee1ed923cfbe9624c8dc11f7ed3f7
Nathan Van Gheem <vangheem@gmail.com>
2011-05-20 15:52:55 +0000

    add utiltiy
    git-svn-id: https://svn.plone.org/svn/collective/collective.vocabularymanager/trunk@239680 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/vocabularymanager/utility.py
@@ -0,0 +1,6 @@
+from collective.vocabularymanager.interfaces import IVocabularyUtility
+from zope.interface import implements
+
+class VocabularyUtility(object):
+    implements(IVocabularyUtility)
+
